1163ff90cd34b3ee8800513ca7c058c20304acf0
Cuishibing <643237029@qq.com>
2021-05-31 00:44:57 +0800

    文件目录

---
+++ gitfunc.js
@@ -0,0 +1,139 @@
+/*
+提交文件到github仓库
+param: {
+    branch: string // 哪个分支，默认master
+    repo: string, // 仓库
+    file: {
+        path: string, // 文件路径
+        mode: string, // file: 文件，direc: 文件夹
+        content: string, // 文件内容，只有mode是file时生效
+    },
+    commitMsg: {
+        name: string, // git name
+        email: string, // git email
+        message: string, // commit message
+    }
+}
+ */
+function commit(param) {
+    let ref = "refs/heads/" + param.branch
+    // 获取head
+    loadRef({
+        repo:param.repo,
+        ref:ref
+    }, (err, xhr, resp)=>{
+        if (err) {
+            return
+        }
+        let headTreeSha = resp.object.sha
+        console.info("headTreeSha:", headTreeSha)
+
+        // 获取tree
+        loadTree({
+            repo: param.repo,
+            sha: headTreeSha
+        }, (err, xhr, resp)=>{
+            if (err) {
+                return
+            }
+            let tree = resp.tree
+
+            console.info("tree:", tree)
+        })
+    })
+}
+
+/*
+加载一个ref
+param:{
+    repo: string, // 仓库
+    ref: string, // ref名称
+}
+ */
+function loadRef(param, callback) {
+    let repo = param.repo
+    let ref = param.ref
+
+    console.info("loadRef: repo:", repo, "ref:", ref)
+
+    reqGithub()("GET", "/repos/" + repo + "/git/" + ref, null, (err, xhr, resp) => {
+        callback(err, xhr, resp)
+    })
+
+}
+
+/*
+更新一个ref
+param:{
+    repo:string,
+    ref:string,
+    data:{
+        sha:string,
+        force:boolean
+    }
+}
+ */
+function updateRef(param) {
+    let repo = param.repo
+    let ref = param.ref
+    let data = param.data
+
+    console.info("updateRef: repo:", repo, "ref:", ref, "data:", data)
+    reqGithub()("PATCH", "/repos/" + repo + "/git/refs/" + ref, data, (err, xhr, resp) => {
+        console.info(resp)
+    })
+}
+
+/*
+加载一个commit
+param:{
+    repo:string,
+    sha:string
+}
+ */
+function loadCommit(param) {
+    let repo = param.repo
+    let sha = param.sha
+
+    console.info("loadCommit: repo:", repo, "sha:", sha)
+    reqGithub()("GET", "/repos/" + repo + "/git/commits/" + sha, null, (err, xhr, resp) => {
+        console.info(resp)
+    })
+}
+
+function loadTree(param, callback) {
+    let repo = param.repo
+    let sha = param.sha
+    console.info("loadTree: repo:", repo, "sha:", sha)
+    reqGithub()("GET", "/repos/" + repo + "/git/trees/" + sha, null, (err, xhr, resp) => {
+        callback(err, xhr, resp)
+    })
+}
+
+/*
+创建一个tree对象
+param:{
+    repo:string,
+    data:{
+        base_tree:string,
+        tree:[
+            {
+                path:string,
+                mode:string, The file mode; one of 100644 for file (blob), 100755 for executable (blob), 040000 for subdirectory (tree), 160000 for submodule (commit), or 120000 for a blob that specifies the path of a symlink.
+                type:string,
+                sha:string,
+                content:string
+            }
+        ]
+    }
+}
+*/
+function createTree(param) {
+    let repo = param.repo
+    let data = param.data
+
+    console.info("createTree: repo:", repo, "data:", data)
+    reqGithub()("POST", "/repos/" + repo + "/git/trees", data, (err, xhr, resp) => {
+        console.info(resp)
+    })
+}
---
+++ requestgithub.js
@@ -0,0 +1,57 @@
+function reqGithub(accessToken, githubHostname) {
+    var timeout = 2000;
+    githubHostname = (githubHostname || 'https://api.github.com');
+    return request;
+
+    function request(method, url, body, callback) {
+      if (typeof body === "function") {
+        callback = body;
+        body = undefined;
+      }
+      else if (!callback) return request.bind(null, method, url, body);
+      var done = false;
+      var json;
+      var xhr = new XMLHttpRequest();
+      xhr.timeout = timeout;
+      xhr.open(method, githubHostname + url, true);
+      if (accessToken) {
+        xhr.setRequestHeader("Authorization", "token " + accessToken);
+      }
+      xhr.setRequestHeader("Accept", "application/vnd.github.v3+json");
+      if (body) {
+        try { json = JSON.stringify(body); }
+        catch (err) { return callback(err); }
+      }
+      xhr.ontimeout = onTimeout;
+      xhr.onerror = function() {
+        callback(new Error("Error requesting " + url));
+      };
+      xhr.onreadystatechange = onReadyStateChange;
+      xhr.send(json);
+
+      function onReadyStateChange() {
+        if (done) return;
+        if (xhr.readyState !== 4) return;
+        // Give onTimeout a chance to run first if that's the reason status is 0.
+        if (!xhr.status) return setTimeout(onReadyStateChange, 0);
+        done = true;
+        var response = {message:xhr.responseText};
+        if (xhr.responseText){
+          try { response = JSON.parse(xhr.responseText); }
+          catch (err) {}
+        }
+        xhr.body = response;
+        return callback(null, xhr, response);
+      }
+
+      function onTimeout() {
+        if (done) return;
+        if (timeout < 8000) {
+          timeout *= 2;
+          return request(method, url, body, callback);
+        }
+        done = true;
+        callback(new Error("Timeout requesting " + url));
+      }
+    }
+  };
