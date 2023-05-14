3494dc92b20d4f7d4b27d0ab9e158ca5b3fc7dc7
cuishibing <643237029@qq.com>
2023-05-15 00:12:37 +0800

    local

--- components/header/header.vue
+++ components/header/header.vue
@@ -3,23 +3,25 @@
     <h1 style="margin:0">不积跬步无以至千里</h1>
     <h5 style="display: inline-block;margin:0"><a href="https://cuishibing.github.io">cuishibing.github.io</a></h5>
     <a href="javascript:void(0)">+</a>
-    <button style="float:right"
-      @click="onSyncFiles">同步</button>
+    <button style="float:right" @click="onSyncFiles">同步</button>
     <hr />
   </div>
-
 </template>
 
-<script>  
+<script>
 import { syncFiles } from '/js/filefactory.js'
 export default {
   methods: {
     onSyncFiles() {
-      syncFiles()
+      this.$load.show()
+      syncFiles().then(data => {
+        this.$load.hide()
+      }).catch(err => {
+        this.$load.hide()
+      })
     }
   }
 }
 </script>
 
-<style>
-</style>
+<style></style>
